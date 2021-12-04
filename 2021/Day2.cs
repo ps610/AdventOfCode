5243a63f3d3a2ebcf8297f55027a6dff9b3614ca
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2021-12-04 13:35:43 +0100

    2021 Day 2

---
+++ 2021/Day2.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2021
+{
+    class Day2
+    {
+        static void Main()
+        {
+            Part1();
+            Part2();
+        }
+
+        private static List<string> ReadInput()
+        {
+            return File.ReadAllLines(@"./2021/Day2_Input.txt").ToList();
+        }
+
+        private static void Part1()
+        {
+            List<string> input = ReadInput();
+            int horizontal = 0;
+            int depth = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                switch (Regex.Match(input[i], @"[^\s\d]+").Value.ToLower())
+                {
+                    case "forward":
+                        horizontal += ExtractIntFromString(input[i]);
+                        break;
+                    case "up":
+                        depth -= ExtractIntFromString(input[i]);
+                        break;
+                    case "down":
+                        depth += ExtractIntFromString(input[i]);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Part 1: horizontal = {horizontal} depth = {depth}");
+            Console.WriteLine($"Part 1: {horizontal * depth}");
+        }
+
+        private static void Part2()
+        {
+            List<string> input = ReadInput();
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                switch (Regex.Match(input[i], @"[^\s\d]+").Value.ToLower())
+                {
+                    case "forward":
+                        horizontal += ExtractIntFromString(input[i]);
+                        depth += aim * ExtractIntFromString(input[i]);
+                        break;
+                    case "up":  
+                        aim -= ExtractIntFromString(input[i]);
+                        break;
+                    case "down":
+                        aim += ExtractIntFromString(input[i]);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Part 2: horizontal = {horizontal} depth = {depth}");
+            Console.WriteLine($"Part 2: {horizontal * depth}");
+        }
+
+        private static int ExtractIntFromString(string input)
+        {
+            return Int32.Parse(Regex.Match(input, @"[^[A-Za-z\s]+").Value);
+        }
+    }
+}
