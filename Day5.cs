e3800402eb46e9a8c920874b24e6f6a9b764adcc
Philipp Stöhr <philipp.stoehr@cosmoconsult.com>
2021-12-07 00:52:14 +0100

    day5 part2, some refactoring

--- Day5.cs
+++ Day5.cs
@@ -6,43 +6,15 @@
         {
             List<(int x1, int y1, int x2, int y2)> coords = DataService.GetDay5Data();
 
-            int xSize = coords.Max(c => Math.Max(c.x1, c.x2));
-            int ySize = coords.Max(c => Math.Max(c.y1, c.y2));
-            int[,] overlappingCoords = new int[xSize + 1, ySize + 1];
+            int xSize = coords.Max(c => Math.Max(c.x1, c.x2)) + 1;
+            int ySize = coords.Max(c => Math.Max(c.y1, c.y2)) + 1;
+            int[,] overlappingCoords = new int[xSize, ySize];
 
-            for (int i = 0; i < coords.Count; i++)
-            {
-                if (coords[i].y1 == coords[i].y2)
-                {
-                    if (coords[i].x1 > coords[i].x2)
-                    {
-                        for (int j = coords[i].x2; j < coords[i].x1 + 1; j++)
-                            overlappingCoords[j, coords[i].y1]++;
-                    }
-                    if (coords[i].x2 > coords[i].x1)
-                    {
-                        for (int j = coords[i].x1; j < coords[i].x2 + 1; j++)
-                            overlappingCoords[j, coords[i].y1]++;
-                    }
-                }
-                if (coords[i].x1 == coords[i].x2)
-                {
-                    if (coords[i].y1 > coords[i].y2)
-                    {
-                        for (int j = coords[i].y2; j < coords[i].y1 + 1; j++)
-                            overlappingCoords[coords[i].x1, j]++;
-                    }
-                    if (coords[i].y2 > coords[i].y1)
-                    {
-                        for (int j = coords[i].y1; j < coords[i].y2 + 1; j++)
-                            overlappingCoords[coords[i].x1, j]++;
-                    }
-                }
-            }
+            findOverlappingCoords(coords, overlappingCoords);
 
             int result = 0;
-            for (int i = 0; i < xSize + 1; i++)
-                for (int j = 0; j < ySize + 1; j++)
+            for (int i = 0; i < xSize; i++)
+                for (int j = 0; j < ySize; j++)
                     if (overlappingCoords[i, j] >= 2) result++;
 
             return result;
@@ -50,9 +22,75 @@
 
         public static int Part2()
         {
-            //List<(int x1, int y1, int x2, int y2)> coords = DataService.GetDay5Data();
+            List<(int x1, int y1, int x2, int y2)> coords = DataService.GetDay5Data();
 
-            return 0;
+            int xSize = coords.Max(c => Math.Max(c.x1, c.x2)) + 1;
+            int ySize = coords.Max(c => Math.Max(c.y1, c.y2)) + 1;
+            int[,] overlappingCoords = new int[xSize, ySize];
+
+            findOverlappingCoords(coords, overlappingCoords, true);
+
+            int result = 0;
+            for (int x = 0; x < xSize; x++)
+                for (int y = 0; y < ySize; y++)
+                    if (overlappingCoords[x, y] >= 2) result++;
+
+            return result;
+        }
+
+        private static void findOverlappingCoords(List<(int x1, int y1, int x2, int y2)> coords, int[,] overlappingCoords, bool includeDiagonals = false)
+        {
+            foreach ((int x1, int y1, int x2, int y2) in coords)
+            {
+                if (y1 == y2) // horizontal
+                {
+                    if (x1 < x2)
+                    {
+                        for (int x = x1; x <= x2; x++)
+                            overlappingCoords[x, y1]++;
+                    }
+                    else
+                    {
+                        for (int x = x2; x <= x1; x++)
+                            overlappingCoords[x, y1]++;
+                    }
+                }
+                else if (x1 == x2) // vertical
+                {
+                    if (y1 < y2)
+                    {
+                        for (int y = y1; y <= y2; y++)
+                            overlappingCoords[x1, y]++;
+                    }
+                    else
+                    {
+                        for (int y = y2; y <= y1; y++)
+                            overlappingCoords[x1, y]++;
+                    }
+                }
+                else if (includeDiagonals) // diagonal
+                {
+                    if (Math.Abs(x1 - x2) != Math.Abs(y1 - y2)) continue;
+                    if (x1 < x2)
+                    {
+                        int y = y1;
+                        for (int x = x1; x <= x2; x++)
+                        {
+                            overlappingCoords[x, y]++;
+                            y += y1 < y2 ? 1 : -1;
+                        }
+                    }
+                    else
+                    {
+                        int y = y2;
+                        for (int x = x2; x <= x1; x++)
+                        {
+                            overlappingCoords[x, y]++;
+                            y += y1 > y2 ? 1 : -1;
+                        }
+                    }
+                }
+            }
         }
     }
 }
